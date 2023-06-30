0956fb00dd7b10de351ce7ed032d4608195745e8
BRYDER <bryan4_alex@hotmail.com>
2023-06-30 16:37:16 -0500

    act

--- server/lib/database/database.js
+++ server/lib/database/database.js
@@ -6,7 +6,5 @@ export const sequelize = new Sequelize("railway", "postgres", "S09Ze05X0LkMeKQpU
   host: "containers-us-west-104.railway.app",
   logging: false,
   dialect: "postgres",
-  port: 6001,
+  port: 6001
 });
-
-
