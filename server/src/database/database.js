5dcc09746ce8d4296f9f8be4a66dbe840bef3400
Jhon Narvaez <jhon.narvaez@espoch.edu.ec>
2023-06-30 15:21:47 -0500

    mi primer commit

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize("recetasdb", "postgres", "12345", {
   host: "localhost",
   dialect: "postgres",
 });
